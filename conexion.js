a9e7a29bc739ab7d4d99d1f05d5750078f0a215c
PaoConcepcion <paolaconcepcion8@gmail.com>
2020-12-15 14:19:46 -0600

    Busquedas de ingredientes y recetas con spoonacular

--- conexion.js
+++ conexion.js
@@ -2,6 +2,6 @@ const mysql = require('mysql');
 module.exports = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Ornelas0299',
+    password: 'root',
     database: 'foodapp'
 });
--- model/apis.js
+++ model/apis.js
@@ -22,6 +22,27 @@ module.exports = {
 
     },
 
+    getIngredientsSpoonacular: async(req) => {
+        return await axios.get(`https://api.spoonacular.com/food/ingredients/search?query=${req}&number=12&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+            .then(data => data)
+            .catch(err => { console.log(err); return err })
+
+    },
+
+    getIngredientSpoonacular: async(req) => {
+        return await axios.get(`https://api.spoonacular.com/food/ingredients/${req}/information?amount=1&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+            .then(data => data)
+            .catch(err => { console.log(err); return err })
+
+    },
+
+    getRecipeQuerySpoonacular: async(req) => {
+        return await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${req}&number=12&apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f`)
+            .then(data => data)
+            .catch(err => { console.log(err); return err })
+
+    },
+
     getRecipesSpoonacular: async(req) => {
         return await axios.get(`https://api.spoonacular.com/recipes/informationBulk?apiKey=0ee5f89c8bb54cfe9dc6fa23c96f8e1f&ids=${req}`)
             .then(data => data)
